6f3c924b83fe3bff48e990b8a53598ccfe96ed29
jfk9w <xp.jfk9w@gmail.com>
2021-10-14 11:38:36 +0300

    upgrade

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ var GitCommit = "dev"
 
 func main() {
 	app.GormDialects["postgres"] = postgres.Open
-	app.Run(GitCommit,
+	app.Run(GitCommit, "HOMEBOT_",
 		tinkoff.Extension{sync.Accounts, sync.TradingOperations, sync.PurchasedSecurities},
 		hassgpx.Extension)
 }
