3fb9874d0f42939d367ca92bf9227f43ee2863b0
jfk9w <xp.jfk9w@gmail.com>
2021-10-20 17:05:54 +0300

    upgrades

--- go.mod
+++ go.mod
@@ -3,10 +3,9 @@ module homebot
 go 1.17
 
 require (
-	github.com/jfk9w-go/flu v0.9.37
-	github.com/jfk9w-go/telegram-bot-api v0.9.40
+	github.com/jfk9w-go/flu v0.10.2
+	github.com/jfk9w-go/telegram-bot-api v0.10.1
 	github.com/pkg/errors v0.9.1
-	github.com/sirupsen/logrus v1.8.1
 	gopkg.in/guregu/null.v3 v3.5.0
 	gorm.io/driver/postgres v1.1.2
 	gorm.io/gorm v1.21.16
@@ -30,12 +29,13 @@ require (
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/prometheus/client_golang v1.11.0 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
-	github.com/prometheus/common v0.31.1 // indirect
+	github.com/prometheus/common v0.32.0 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519 // indirect
 	golang.org/x/exp v0.0.0-20211012155715-ffe10e552389 // indirect
 	golang.org/x/net v0.0.0-20211014222326-fd004c51d1d6 // indirect
-	golang.org/x/sys v0.0.0-20211013075003-97ac67df715c // indirect
+	golang.org/x/sys v0.0.0-20211020064051-0ec99a608a1b // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
--- main.go
+++ main.go
@@ -1,41 +1,77 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"homebot/hassgpx"
 	"homebot/tinkoff"
 	"homebot/tinkoff/sync"
 
 	"github.com/jfk9w-go/flu"
+	fluapp "github.com/jfk9w-go/flu/app"
 	tgapp "github.com/jfk9w-go/telegram-bot-api/ext/app"
-	"github.com/sirupsen/logrus"
+	"github.com/pkg/errors"
 	"gorm.io/driver/postgres"
 )
 
 var GitCommit = "dev"
 
 func main() {
-	tgapp.GormDialects["postgres"] = postgres.Open
-	configurer := tgapp.DefaultConfigurer("HOMEBOT_", nil, "config.file", "config.stdin")
-	app, err := tgapp.Create(GitCommit, flu.DefaultClock, configurer)
-	if err != nil {
-		logrus.Fatal(err)
-	}
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
 
+	fluapp.GormDialects["postgres"] = postgres.Open
+	app := tgapp.Create(GitCommit, flu.DefaultClock)
 	defer flu.CloseQuietly(app)
-	if ok, err := app.Show(); err != nil {
-		logrus.Fatal(err)
-	} else if ok {
-		return
-	}
-
-	if ok, err := tinkoff.Run(app); err != nil {
-		logrus.Fatal(err)
-	} else if ok {
-		return
-	}
-
-	tgapp.Run(app,
+	app.ApplyExtensions(
 		tinkoff.Extension{sync.Accounts, sync.TradingOperations, sync.PurchasedSecurities},
 		hassgpx.Extension,
 	)
+
+	fluapp.Run(ctx, Instance{app}, fluapp.DefaultConfigurer("homebot"))
+	flu.AwaitSignal()
+}
+
+type Instance struct {
+	*tgapp.Instance
+}
+
+func (app Instance) Show() (bool, error) {
+	if done, err := app.Instance.Show(); err != nil || done {
+		return done, err
+	}
+
+	globalConfig := new(struct {
+		Tinkoff struct {
+			Show, Generate bool
+			Data           flu.File
+			Credentials    tinkoff.CredentialStore
+		}
+	})
+
+	if err := app.GetConfig().As(globalConfig); err != nil {
+		return false, errors.Wrap(err, "get config")
+	}
+
+	config := globalConfig.Tinkoff
+	if config.Show {
+		creds, err := tinkoff.DecodeCredentialsFrom(config.Data)
+		if err != nil {
+			return false, errors.Wrap(err, "decode credentials")
+		}
+
+		separator := "------------"
+		println(separator)
+		for key, cred := range creds {
+			fmt.Printf("id: %d\nusername: %s\npassword: %s\n%s\n", key, cred.Username, cred.Password, separator)
+		}
+
+		return true, nil
+	}
+
+	if config.Generate {
+		return true, errors.Wrap(config.Credentials.EncodeTo(config.Data), "generate")
+	}
+
+	return false, nil
 }
